0f59f9ee95c256fe9c182031e5c4e9d71f3cd8cc
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-03-06 15:42:31 -0500

    complete

---
+++ enemy.py
@@ -0,0 +1,15 @@
+class Enemy:
+    monsters = {
+    "Goblin": {"health": 80, "damage": 5, "gold": 10},
+    "Troll": {"health": 100, "damage": 10, "gold": 25},
+    "WereWolf": {"health": 150, "damage": 15, "gold": 30},
+    "Vampire": {"health": 180, "damage": 20, "gold": 50},
+    "Dragon": {"health": 200, "damage": 25, "gold": 60}
+    }
+
+    boss = {
+        "name": "The Dark Lord",
+        "health": 1000,
+        "damage": 30,
+        "gold": 1003
+    }
---
+++ weapons.py
@@ -0,0 +1,7 @@
+class Weapon:
+    weapons = {
+    "Dagger": {"price": 25, "damage": 15},
+    "Sword": {"price": 50, "damage": 20},
+    "Axe": {"price": 100, "damage": 25},
+    "Bow": {"price": 200, "damage": 30},
+}
